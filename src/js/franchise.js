8b03f3e15da7f8405f76196389c85d184efb1979
E-Molostova <molostova8973@gmail.com>
2021-07-27 17:34:53 +0300

    contacts fix+modal

--- src/js/franchise.js
+++ src/js/franchise.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-franchise-modal-open]'),
-    closeModalBtn: document.querySelector('[data-franchise-modal-close]'),
-    modal: document.querySelector('[data-franchise-modal]'),
+    openfranchise: document.querySelector('[data-franchise-modal-open]'),
+    closefranchise: document.querySelector('[data-franchise-modal-close]'),
+    modalfranchise: document.querySelector('[data-franchise-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openfranchise.addEventListener('click', togglefranchise);
+  refs.closefranchise.addEventListener('click', togglefranchise);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  function togglefranchise() {
+    refs.modalfranchise.classList.toggle('is-hidden');
   }
 })();
--- src/js/locations.js
+++ src/js/locations.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-locations-modal-open]'),
-    closeModalBtn: document.querySelector('[data-locations-modal-close]'),
-    modal: document.querySelector('[data-locations-modal]'),
+    openlocations: document.querySelector('[data-locations-modal-open]'),
+    closelocations: document.querySelector('[data-locations-modal-close]'),
+    modallocations: document.querySelector('[data-locations-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openlocations.addEventListener('click', togglelocations);
+  refs.closelocations.addEventListener('click', togglelocations);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  function togglelocations() {
+    refs.modallocations.classList.toggle('is-hidden');
   }
 })();
