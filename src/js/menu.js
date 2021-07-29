ece1716a4d68415049ff8f1ca645f668facf1d00
Artem Khmeliuk <bigsmash@ukr.net>
2021-07-29 03:11:45 +0300

    to-buy added

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,6 +3,8 @@
     openMenuBtn: document.querySelector('.mob-menu-open-btn'),
     closeMenuBtn: document.querySelector('.mob-menu__close-btn'),
     menu: document.querySelector('.mob-menu'),
+    menua: document.querySelector('.header-menu'),
+     body: document.querySelector('[data-js-body]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -10,5 +12,8 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('visually-hidden');
+    refs.menua.classList.toggle('menu-active');
+    refs.body.classList.toggle('no-scroll');
   }
+ 
 })();
